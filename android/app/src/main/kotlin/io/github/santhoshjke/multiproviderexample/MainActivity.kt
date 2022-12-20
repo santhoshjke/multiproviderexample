2b2eea79ac4d030b4d4779d593c991937d8baf36
San <sandevwork@gmail.com>
2022-12-20 07:42:21 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/io/github/santhoshjke/multiproviderexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.santhoshjke.multiproviderexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+String now() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+
+  Seconds() : value = now();
+}
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        color: Colors.yellow,
+        height: 100,
+        child: Text(seconds.value),
+      ),
+    );
+  }
+}
+
+@immutable
+class Minutes {
+  final String value;
+
+  Minutes() : value = now();
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        color: Colors.blue,
+        height: 100,
+        child: Text(minutes.value),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+              const Duration(seconds: 5),
+              (_) => Minutes(),
+            ),
+            initialData: Minutes(),
+          )
+        ],
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                SecondsWidget(),
+                MinutesWidget(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
